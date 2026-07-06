b848ceda0fd3bafe46b3e22002294781401ed2aa
Muhammad Ismaeel Qazi <ismaeelqazi30@gmail.com>
2026-07-06 23:46:52 +0500

    Update Flask import and change home route response

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
-from Flask import flask ,render_template 
+from flask import Flask, send_from_directory
+import os
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-  return render_template("OneKeyReaction.html")
+    return send_from_directory(os.getcwd(), "OneKeyReaction.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
